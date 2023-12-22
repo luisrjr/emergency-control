eca2211231b7d788966964aecedc774bba529940
Luis Rodriguez <luisrodriguezjr@gmail.com>
2023-12-21 21:41:18 -0800

    updated emergencies array

--- src/App.js
+++ src/App.js
@@ -2,41 +2,51 @@ import React, { useState, useEffect } from "react";
 //import Countdown from 'react-countdown';
 import './App.css';
 
-const emergencies = ["House Fire Emergency", "Forest Fire Emergency", "Earthquake Emergency"];
+const emergencies = [{
+  name: "House Fire Emergency"
+  }, 
+  {
+    name:"Forest Fire Emergency"
+  }, 
+  {
+    name:"Earthquake Emergency"
+  }];
 
 
 const App = () => {
 
   const EmergButtons = (props) => {
-
+    
     return (
       <button>{props.name}</button>
     );
   }
 
- const Timer = () => {
-  const [time, updateTime] = useState(241);
+  const Timer = () => {
+    const [time, updateTime] = useState(10);
 
-  const countdown = () => {
-    return(
-    updateTime(time - 1)
+    const countdown = () => {
+      return (
+        updateTime(time - 1)
+      )
+    }
+
+    useEffect(() => {
+      const countdownInterval = setInterval(countdown, 1000);
+      return () => {if(time <= 0) 
+        clearInterval(countdownInterval);
+      }
+    },);
+
+    return (
+      <h3>{time}</h3>
     )
+
   }
-
-  useEffect(() => {
-    const countdownInterval = setInterval(countdown, 1000);
-    return () => clearInterval(countdownInterval);
-  }, );
-
-  return (
-    <h3>{time}</h3>
-  )
-
- }
 
 
   const DisplayEmergency = () => {
-    const [emergency, updateEmergency] = useState("");
+    const [emergency, updateEmergency] = useState("Building fire");
 
     const randomEmergency = () => {
       const index = Math.floor(Math.random() * emergencies.length);
@@ -49,7 +59,7 @@ const App = () => {
       const intervalID = setInterval(randomEmergency, 10000);
       return () => clearInterval(intervalID);
     }, []);
-       return (
+    return (
       <h1>{emergency}</h1>
     );
   }
@@ -68,6 +78,7 @@ const App = () => {
             <EmergButtons name="Ambulance" />
             <EmergButtons name="Helicopter" />
             <EmergButtons name="Police" />
+            <EmergButtons name="Start" onclick={() => DisplayEmergency()}/>
           </div>
         </div>
         <div className="countdown-display">
