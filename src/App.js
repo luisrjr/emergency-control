e0fab2ad08a8e382af5a6194ef45751bcf64b48c
Luis Rodriguez <luisrodriguezjr@gmail.com>
2023-12-29 21:33:58 -0800

    From scratch again to do the displays only

--- src/App.js
+++ src/App.js
@@ -1,63 +1,75 @@
-import React, {useState} from "react";
+import React from "react";
 //import Countdown from 'react-countdown';
 import './App.css';
 
 const emergencies = [{
-    "name": "House Fire Emergency",
-    "message": "Put water on the fire"
-    },
-    {
-    "name":"Forest Fire Emergency",
-    "message":"Extinguish the forest fire"
-    },
-    {
-    "name":"Earthquake Emergency",
-    "message": "There's been an earthquake, call the police!'"
-    }];
+  "name": "House Fire Emergency",
+  "message": "Put water on the fire"
+  },
+  {
+  "name":"Forest Fire Emergency",
+  "message":"Extinguish the forest fire"
+  },
+  {
+  "name":"Earthquake Emergency",
+  "message": "There's been an earthquake, call the police!'"
+  }];
 
-const time = [240, 100, 400, 50]
+  const countdownTimes = [240, 100, 200, 150, 250, 60, 350, 75, 30];
+  
+function DisplayEmergency() {
+  let index = Math.floor(Math.random() * emergencies.length);
+  let emergency = emergencies[index].name
+  return (
+  <h1>{emergency}</h1>
+  )
+}
 
-
+function DisplayCountdown() {
+  let timeIndex = Math.floor(Math.random() * countdownTimes.length);
+  let countdown = countdownTimes[timeIndex];
+  return (
+    <h1>{countdown}</h1>
+  )
+}
+  
 
 const App = () => {
-  const [emergency, setEmergency] = useState("");
+  
+    //
 
+  
 
+  //
+  //let emergency = emergencies[index];
+ 
 
-const handleClick = () => {
-  let index = Math.floor(Math.random() * emergencies.length);
-  setEmergency(emergencies[index]);
-}
-
-const Timer = () => {
-  let timeIndex = Math.floor(Math.random() * time.length);
-  let countdown = time[timeIndex];
-   if (countdown <= 0) {
-     countdown -= 1;
-   }
-    return (
-      <div>
-      <span>{countdown}</span>
-      </div>
-    )
-}
-
+  
+  /*
+    const Timer = () => {
+      const [time, setTime] = useState();
+        useEffect(() => {
+          
+          
+          const intervalId = setInterval(() => {
+            setTime(timeIndex - 1); 
+          }, 1000)
+          return () => {
+            clearInterval(intervalId);
+            <h2>{time}</h2>
+          }
+        }, [time]); 
+      }
+      */
 
   return (
-    <div className="App">
-      <body>
-        <div>
-          <h1>{emergency.name}</h1>
-        </div>
-          <div className="button-grid">
-            <button onClick={handleClick}>Start</button>
+    <div>
+          <DisplayEmergency />
+          <div>
+          <DisplayCountdown />
           </div>
-           <div id="emergency-message">
-            {emergency.message}
-        </div> 
-        <Timer />
-      </body>
-    </div>
+   </div>
+           
   );
 }
 
