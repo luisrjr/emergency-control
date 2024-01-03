eb3fc8a6f7b09e035558f99e46f9c4b248955498
Luis Rodriguez <luisrodriguezjr@gmail.com>
2024-01-02 21:13:28 -0800

    moved countdown to components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-//import Countdown from 'react-countdown';
+import React from "react";
+import { CountDown } from "./components";
 import "./App.css";
 
 const emergencies = [
@@ -17,27 +17,17 @@ const emergencies = [
   },
 ];
 
-const countdownTimes = [240, 100, 200, 150, 250, 60, 350, 75, 30];
 
 const App = () => {
   let index = Math.floor(Math.random() * emergencies.length);
   let emergency = emergencies[index].name;
 
-  const [seconds, setSeconds] = useState(
-    countdownTimes[Math.floor(Math.random() * countdownTimes.length)],
-  );
-  //let timeIndex = Math.floor(Math.random() * countdownTimes.length);
-  //let countdown = countdownTimes[timeIndex];
-
-  useEffect(() => {
-    seconds > 0 && setTimeout(() => setSeconds(seconds - 1), 1000);
-  }, [seconds]);
 
   return (
     <div>
       <h1>{emergency}</h1>
       <div>
-        <h3>{seconds}</h3>
+        <CountDown />
       </div>
     </div>
   );
--- src/components.jsx
+++ src/components.jsx
@@ -1,2 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+
+export const CountDown = () => {
+    const countdownTimes = [240, 100, 200, 150, 250, 60, 350, 75, 30];
+
+const [seconds, setSeconds] = useState(
+    countdownTimes[Math.floor(Math.random() * countdownTimes.length)],
+  );
+  //let timeIndex = Math.floor(Math.random() * countdownTimes.length);
+  //let countdown = countdownTimes[timeIndex];
+
+  useEffect(() => {
+    seconds > 0 && setTimeout(() => setSeconds(seconds - 1), 1000);
+  }, [seconds]);
+
+  return (
+    <h3>{seconds}</h3>
+  )
+}
 
