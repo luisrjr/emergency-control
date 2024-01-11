269d3d4e39e0bd3f52132cbc25030059b33fa5ea
Luis Rodriguez <luisrodriguezjr@gmail.com>
2024-01-11 11:51:12 -0800

    sync 8

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import React from "react";
-import { CountDown } from "./components";
+//import { CountDown } from "./components";
 import "./App.css";
-
-const emergencies = [
-  {
-    name: "Fire Emergency",
-    message: "Put water on the fire",
-  },
-  {
-    name: "Forest Fire Emergency",
-    message: "Extinguish the forest fire",
-  },
-  {
-    name: "Earthquake Emergency",
-    message: "There's been an earthquake, call the police!'",
-  },
-];
 
 
 const App = () => {
+  
+
+  const emergencies = [
+    {
+      name: "Fire Emergency",
+      message: "Put water on the fire",
+    },
+    {
+      name: "Forest Fire Emergency",
+      message: "Extinguish the forest fire",
+    },
+    {
+      name: "Earthquake Emergency",
+      message: "There's been an earthquake, call the police!'",
+    },
+  ];
+
   let index = Math.floor(Math.random() * emergencies.length);
   let emergency = emergencies[index].name;
-
 
   return (
     <div>
       <h1>{emergency}</h1>
       <div>
-        <CountDown />
+        <h2>Countdown</h2>
       </div>
     </div>
   );
